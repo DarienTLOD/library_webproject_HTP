bf0e467ae4c007f2ef98a974352c5ae2fbe7815c
DarienTLOD <darienyry@gmail.com>
2016-09-02 21:18:51 +0300

    Added gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,52 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['copy_css', 'copy_js']);
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var uncss = require('gulp-uncss');
+var clean = require('gulp-clean');
 
-gulp.task('copy_js', function() {
+gulp.task('default', ['copy_css', 'copy_js','scripts','styles']);
+
+gulp.task('copy_js', function () {
     gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js', 'bower_components/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('web/js/vendor/'));
     gulp.src(['bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'])
         .pipe(gulp.dest('web/js/vendor/'));
     gulp.src(['bower_components/bootstrap-validator/dist/validator.min.js'])
         .pipe(gulp.dest('web/js/vendor/'));
+    gulp.src(['bower_components/bootstrap-select/dist/js/bootstrap-select.min.js'])
+        .pipe(gulp.dest('web/js/vendor/'));
 });
 
-gulp.task('copy_css', function(){
+gulp.task('copy_css', function () {
     gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
         .pipe(gulp.dest('web/css/vendor/'));
     gulp.src(['bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css'])
+        .pipe(gulp.dest('web/css/vendor/'));
+    gulp.src(['bower_components/bootstrap-select/dist/css/bootstrap-select.min.css'])
         .pipe(gulp.dest('web/css/vendor/'));
     gulp.src(['bower_components/bootstrap/dist/fonts/*.*'])
         .pipe(gulp.dest('web/css/fonts/'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('css/*.css', ['default']);
 });
+
+gulp.task('styles', function() {
+   gulp.src(['web/css/*.css','!web/css/*.min.css'])
+        .pipe(uncss({html: ['web/*.html']
+        }))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('web/css/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(['web/js/*.js','!web/js/*.min.js'])
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('web/js/'));
+    });
+
